217adfb490d04ab4e487fd21b3ccb745582760ff
kwangminNam <johnpower@naver.com>
2024-12-31 23:22:41 +0900

    fix: fixed sst config

--- packages/web/sst.config.ts
+++ packages/web/sst.config.ts
@@ -15,8 +15,15 @@ export default {
       const path = envPathMap.get(stage);
 
       const { parsed: environment } = dotenv.config({ path });
+      console.log('Loaded environment:', environment);
       const site = new NextjsSite(stack, "site", {
-        environment,
+        environment: {
+          ...environment,
+          NEXT_PUBLIC_GITHUB_COMMENT_REPO_ID: process.env.NEXT_PUBLIC_GITHUB_COMMENT_REPO_ID || '',
+          NEXT_PUBLIC_GITHUB_COMMENT_REPO: process.env.NEXT_PUBLIC_GITHUB_COMMENT_REPO || '',
+          NEXT_PUBLIC_GITHUB_COMMENT_CATEGORY: process.env.NEXT_PUBLIC_GITHUB_COMMENT_CATEGORY || '',
+          NEXT_PUBLIC_GITHUB_COMMENT_CATEGORY_ID: process.env.NEXT_PUBLIC_GITHUB_COMMENT_CATEGORY_ID || '',
+        },
         customDomain: {
           domainName: "kwangmin-nam.com",
           domainAlias: "www.kwangmin-nam.com",
