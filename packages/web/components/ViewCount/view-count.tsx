199d646178ef8395174122b2d674a1b4585ce4c6
kwangminNam <johnpower@naver.com>
2025-01-15 21:34:29 +0900

    feat:add no store

--- packages/web/components/ViewCount/view-count.tsx
+++ packages/web/components/ViewCount/view-count.tsx
@@ -1,14 +1,14 @@
 import { getViewCountAction } from "@/action/action";
 import ViewCountClient from "./increase-view";
+import { unstable_noStore as noStore } from "next/cache";
 
 const ViewCount: React.FC<{
   slug: string;
 }> = async ({ slug }) => {
+  noStore();
   const initialViews = await getViewCountAction(slug);
 
-  return (
-    <ViewCountClient slug={slug} initialViews={initialViews} />
-  )
+  return <ViewCountClient slug={slug} initialViews={initialViews} />;
 };
 
 export default ViewCount;
