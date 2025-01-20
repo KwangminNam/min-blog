ce9e025d9aeebcd2680cdc5fd41dae84acad192d
남광민 <kwangmin.nam@voithru.com>
2025-01-20 14:27:45 +0900

    feat:modfied mount

--- packages/web/provider/theme-provider.tsx
+++ packages/web/provider/theme-provider.tsx
@@ -3,22 +3,21 @@
 import { THEME } from "@/constant/general";
 import useMount from "@/hooks/useMount";
 import { darkTheme, lightTheme } from "@monorepo/ui";
-import { ThemeProvider, useTheme } from "next-themes";
+import { ThemeProvider } from "next-themes";
 import { PropsWithChildren, useEffect, useState } from "react";
 
 interface ThemeProviderProps extends PropsWithChildren {}
 
 const Provider = ({ children }: ThemeProviderProps) => {
-  // const { resolvedTheme } = useTheme();
-  // const [mounted, setMounted] = useState(false);
+  const [mounted, setMounted] = useState(false);
 
-  // useMount(() => {
-  //   setMounted(true);
-  // });
+  useMount(() => {
+    setMounted(true);
+  });
 
-  // if (!mounted && resolvedTheme === THEME.light) {
-  //   return <>{children}</>;
-  // }
+  if (!mounted) {
+    return null;
+  }
   return (
     <ThemeProvider
       attribute="class"
